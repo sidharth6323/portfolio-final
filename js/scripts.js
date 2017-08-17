e429a4a287cfd385af87493d46d3dae5a287caea
SIDHARTH KUMAR <sidharth6323@gmail.com>
2017-08-18 04:14:47 +0530

    leftbar js completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-	$('#fullpage').fullpage();
+	$('#fullpage').fullpage({
+		anchors: ['section1', 'section2', 'section3', 'section4'],
+		onLeave: function(index, nextIndex, direction){
+				var leavingSection = $(this);
+				$('li').removeClass("selected");
+				console.log(index,nextIndex);
+				switch(nextIndex)
+				{
+					case 1:$('.about').addClass("selected");break;
+					case 2:$('.expertise').addClass("selected");break;
+					case 3:$('.work').addClass("selected");break;
+					case 4:$('.projects').addClass("selected");break;
+					case 5:$('.hire').addClass("selected");break;
+				}
+
+		}
+	});
+	$(window).scroll(function(){
+		console.log("scrolled");
+	})
 });
